6dccf4a149e1d973c0358e5a5c1f8e56a7a9c610
Cyril Pfaff <cp512@cylian.org>
2022-11-17 16:09:58 +0100

    MàJ 6

--- src/main/java/fr/cylian/hello/Outil.java
+++ src/main/java/fr/cylian/hello/Outil.java
@@ -8,6 +8,6 @@ public class Outil
 {
     public static void hello( String who )
     {
-        System.out.println( "Hello (v20211206) - " + who );
+        System.out.println( "Hello (v20221116) - " + who );
     }
 }
